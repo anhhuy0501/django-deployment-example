fb5ec340c58b4e8804e984227ef4e3b18811b9fd
anhhuy0501 <anhhuy0501@gmail.com>
2020-01-05 16:13:25 +0900

    replace django 2 with 1 version

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context={'text':"hello world", 'number':100}
-    return render(request,'basic_app/index.html', context)
+    return render(request,'basic_app/index.html')
 
 def other(request):
     return render(request,'basic_app/other.html')
